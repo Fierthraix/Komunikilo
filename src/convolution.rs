fd8169add9af26781df60409532e5df78c8a4996
Lucas Rooyakkers <lucas.rooyakkers@pm.me>
2023-09-06 22:49:37 -0400

    Added a split-stream convolver for QPSK.

--- src/convolution.rs
+++ src/convolution.rs
@@ -24,6 +24,111 @@ pub fn convolve_linear(signal: Vec<f64>, filter: Vec<f64>) -> Vec<f64> {
     out
 }
 
+pub struct Convolver2<T, I>
+where
+    I: Iterator<Item = (T, T)>,
+    T: Mul<T, Output = T> + Sum<T> + Copy,
+{
+    source: I,
+    filter: Vec<T>,
+    buffer1: VecDeque<T>,
+    buffer2: VecDeque<T>,
+}
+
+impl<T, I> Convolver2<T, I>
+where
+    I: Iterator<Item = (T, T)>,
+    T: Mul<T, Output = T> + Sum<T> + Copy,
+{
+    pub fn new(source: I, filter: Vec<T>) -> Convolver2<T, I> {
+        let filter_len = filter.len();
+        Convolver2 {
+            source,
+            filter,
+            buffer1: VecDeque::with_capacity(filter_len + 1),
+            buffer2: VecDeque::with_capacity(filter_len + 1),
+        }
+    }
+
+    // pub fn push(&mut self) {}
+
+    pub fn convolve(&self) -> (T, T) {
+        (
+            self.buffer1
+                .iter()
+                .zip(self.filter.iter().rev())
+                .map(|(&buf, &filt)| buf * filt)
+                .sum(),
+            self.buffer2
+                .iter()
+                .zip(self.filter.iter().rev())
+                .map(|(&buf, &filt)| buf * filt)
+                .sum(),
+        )
+    }
+}
+
+pub fn convolve2<T, I>(signal: I, filter: Vec<T>) -> impl Iterator<Item = (T, T)>
+where
+    I: Iterator<Item = (T, T)>,
+    T: Mul<T, Output = T> + Sum<T> + Copy,
+{
+    Convolver2::new(signal, filter)
+}
+
+/*
+pub fn convolve2<T>(signal: I, signal2: T, filter: Vec<T>) -> impl Iterator<Item = (T, T)>
+where
+    I: Iterator<Item = (T, T)>,
+    T: Mul<T, Output = T> + Sum<T> + Copy,
+{
+    let c1 = Convolver2::new(filter);
+    let c2 = Convolver2::new(filter);
+}
+*/
+
+impl<T, I> Iterator for Convolver2<T, I>
+where
+    I: Iterator<Item = (T, T)>,
+    T: Mul<T, Output = T> + Sum<T> + Copy,
+{
+    type Item = (T, T);
+
+    fn next(&mut self) -> Option<(T, T)> {
+        match self.source.next() {
+            Some(nums) => {
+                // Here is where the convolution happens.
+
+                let (num1, num2) = nums;
+
+                self.buffer1.push_front(num1);
+                self.buffer2.push_front(num2);
+
+                if self.buffer1.len() > self.filter.len() {
+                    // Deal with front, before buffer is filled to capacity.
+                    self.buffer1.pop_back();
+                }
+                if self.buffer2.len() > self.filter.len() {
+                    // Deal with front, before buffer is filled to capacity.
+                    self.buffer2.pop_back();
+                }
+
+                Some(self.convolve())
+            }
+            None => {
+                // if self.buffer.is_empty() {
+                if self.buffer1.len() <= 1 || self.buffer2.len() <= 1 {
+                    None
+                } else {
+                    self.buffer1.pop_back();
+                    self.buffer2.pop_back();
+                    Some(self.convolve())
+                }
+            }
+        }
+    }
+}
+
 pub fn convolve<T, I>(signal: I, filter: Vec<T>) -> impl Iterator<Item = T>
 where
     I: Iterator<Item = T>,
