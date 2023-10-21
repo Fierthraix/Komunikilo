294a02f83f58c28abefbe2eba9a9e133a44892bb
Lucas Rooyakkers <lucas.rooyakkers@pm.me>
2023-10-21 14:38:59 -0400

    Made logistic map into iterator.

---
+++ src/logistic_map.rs
@@ -0,0 +1,24 @@
+#[inline]
+pub fn logistic_map(mu: f64, x0: f64) -> f64 {
+    mu * x0 * (1f64 - x0)
+}
+
+pub struct LogisticMap {
+    mu: f64,
+    xn: f64,
+}
+
+impl LogisticMap {
+    pub fn new(mu: f64, x0: f64) -> Self {
+        Self { mu, xn: x0 }
+    }
+}
+
+impl Iterator for LogisticMap {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<f64> {
+        self.xn = logistic_map(self.mu, self.xn);
+        Some(self.xn)
+    }
+}
