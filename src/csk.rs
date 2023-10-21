f8209b0a22bb44fc1440d07ca84ee98295d8f8e3
Lucas Rooyakkers <lucas.rooyakkers@pm.me>
2023-10-21 14:39:44 -0400

    Refactored CSK code to be tiny.

--- src/csk.rs
+++ src/csk.rs
@@ -1,125 +1,29 @@
-use crate::{logistic_map, Bit};
+use crate::{logistic_map::LogisticMap, Bit};
 
 fn tx_baseband_csk<I>(message: I) -> impl Iterator<Item = f64>
 where
     I: Iterator<Item = Bit>,
 {
-    // Multiply each branch by either one or the other chaos generator.
-
-    struct Csk<I>
-    where
-        I: Iterator<Item = Bit>,
-    {
-        source: I,
-        curr_a: f64,
-        curr_b: f64,
-    }
-
-    impl<I> Csk<I>
-    where
-        I: Iterator<Item = Bit>,
-    {
-        fn new(source: I) -> Csk<I> {
-            Self {
-                source,
-                curr_a: 0.1,
-                curr_b: 0.15,
-            }
-        }
-    }
-
-    impl<I> Iterator for Csk<I>
-    where
-        I: Iterator<Item = Bit>,
-    {
-        type Item = f64;
-
-        fn next(&mut self) -> Option<f64> {
-            self.curr_a = logistic_map(3.9, self.curr_a);
-            self.curr_b = logistic_map(3.9, self.curr_b);
-            match self.source.next() {
-                Some(bit) => {
-                    if bit {
-                        Some(self.curr_a)
-                    } else {
-                        Some(self.curr_b)
-                    }
-                }
-                None => None,
-            }
-        }
-    }
-
-    trait CskExt: Iterator {
-        fn csk_tx(self) -> Csk<Self>
-        where
-            Self: Iterator<Item = Bit> + Sized,
-        {
-            Csk::new(self)
-        }
-    }
-
-    impl<I: Iterator> CskExt for I {}
-
-    message.csk_tx()
+    let mu = 3.9;
+    let x0_1 = 0.1;
+    let x0_2 = 0.15;
+    message
+        .zip(LogisticMap::new(mu, x0_1))
+        .zip(LogisticMap::new(mu, x0_2))
+        .map(|((bit, chaos_1), chaos_2)| if bit { chaos_1 } else { chaos_2 })
 }
 
 fn rx_baseband_csk<I>(message: I) -> impl Iterator<Item = Bit>
 where
     I: Iterator<Item = f64>,
 {
-    // Multiply each branch by either one or the other chaos generator.
-
-    struct Csk<I>
-    where
-        I: Iterator<Item = f64>,
-    {
-        source: I,
-        curr_a: f64,
-        curr_b: f64,
-    }
-
-    impl<I> Csk<I>
-    where
-        I: Iterator<Item = f64>,
-    {
-        fn new(source: I) -> Csk<I> {
-            Self {
-                source,
-                curr_a: 0.1,
-                curr_b: 0.15,
-            }
-        }
-    }
-
-    impl<I> Iterator for Csk<I>
-    where
-        I: Iterator<Item = f64>,
-    {
-        type Item = Bit;
-
-        fn next(&mut self) -> Option<Bit> {
-            self.curr_a = logistic_map(3.9, self.curr_a);
-            self.curr_b = logistic_map(3.9, self.curr_b);
-            match self.source.next() {
-                Some(num) => Some((num - self.curr_a).abs() < (num - self.curr_b).abs()),
-                None => None,
-            }
-        }
-    }
-
-    trait CskExt: Iterator {
-        fn csk_rx(self) -> Csk<Self>
-        where
-            Self: Iterator<Item = f64> + Sized,
-        {
-            Csk::new(self)
-        }
-    }
-
-    impl<I: Iterator> CskExt for I {}
-
-    message.csk_rx()
+    let mu = 3.9;
+    let x0_1 = 0.1;
+    let x0_2 = 0.15;
+    message
+        .zip(LogisticMap::new(mu, x0_1))
+        .zip(LogisticMap::new(mu, x0_2))
+        .map(|((sample, chaos_1), chaos_2)| (sample - chaos_1).abs() < (sample - chaos_2).abs())
 }
 
 #[cfg(test)]
